4d1e15cc830bf477e81bdfad3a43090552ae51f5
Yadvendra Sharma <98981156+yoda-76@users.noreply.github.com>
2024-08-16 03:00:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ function triggerFunction() {
 }
 
 // Schedule the function to run at 9:30 AM every day
-cron.schedule('30 21 * * *', async () => {
+cron.schedule('32 21 * * *', async () => {
   await client.set("market_open",1)
   console.log("Market Opened");
   await client.set("squared_off",0)
